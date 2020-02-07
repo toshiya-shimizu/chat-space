1e175e1210a24f589319c456b794cbfb3b4c97af
toshiya-shimizu <shcevchenko0718@yahoo.co.jp>
2020-02-07 21:10:14 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root "groups#index"
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :create, :edit, :update, :new]
 end
+
