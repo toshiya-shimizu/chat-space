dd5eaa23f6a7da4098bd009a0093b7ad88201eaa
toshiya-shimizu <shcevchenko0718@yahoo.co.jp>
2020-02-21 14:24:49 +0900

    cimmit

--- config/application.rb
+++ config/application.rb
@@ -6,15 +6,18 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module ChatSpace
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
     config.generators do |g|
+      
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
-      # config.i18n.default_locale = :ja
+     
+     
     end
   end
 end
-
