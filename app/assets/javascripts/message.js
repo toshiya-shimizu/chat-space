fc5e5cefc76351e5c25a82ba31720d77406dfaf4
toshiya-shimizu <shcevchenko0718@yahoo.co.jp>
2020-02-26 21:17:54 +0900

    commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -52,6 +52,7 @@ $('#new_message').on('submit', function(e){
    contentType: false
  })
   .done(function(data){
+    console.log(data)
     var html = buildHTML(data);
     $('.messages').append(html);
     $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');         
@@ -64,4 +65,30 @@ $('#new_message').on('submit', function(e){
     $('.form__submit').prop('disabled',false);
   });
 });
-})
+var reloadMessages = function() {
+  var last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    url: "api/messages",
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    console.log(messages)
+    if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    }
+  })
+  .fail(function() {
+    alert('error');
+  });
+};
+if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+}
+});
